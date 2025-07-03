674f06e323a95c793b841d3b37f539046bfb49e1
unknown <jwon96.park@gmail.com>
2025-07-03 15:50:02 +0900

    [refactoring] use const int instead of magic number

--- Project18/device_driver.cpp
+++ Project18/device_driver.cpp
@@ -7,12 +7,17 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     int readValue = (int)(m_hardware->read(address));
-    for (int i = 0; i < 4; i++) {
+    posetConditionCheck(address, readValue);
+    return readValue;
+}
+
+void DeviceDriver::posetConditionCheck(const long address, const int readValue)
+{
+    for (int turn = 1; turn <= REPEAT_NUM; turn++) {
         int repeatedReadValue = (int)(m_hardware->read(address));
         if (repeatedReadValue != readValue)
             throw std::runtime_error("Read value is different. Error!");;
     }
-    return readValue;
 }
 
 void DeviceDriver::write(long address, int data)
--- Project18/device_driver.h
+++ Project18/device_driver.h
@@ -6,8 +6,10 @@ class DeviceDriver
 public:
     DeviceDriver(FlashMemoryDevice* hardware);
     int read(long address);
+    void posetConditionCheck(long address, int readValue);
     void write(long address, int data);
 
 protected:
     FlashMemoryDevice* m_hardware;
+    const int REPEAT_NUM = 4;
 };
